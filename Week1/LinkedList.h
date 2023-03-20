a312e22675530aa0c69d90f88135f1de4c1ac19a
WarpSpace <lsxestelle@icloud.com>
2023-03-20 12:42:37 +0800

    双向链表的参考（无异常处理）

--- Week1/LinkedList.h
+++ Week1/LinkedList.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iterator>
+#include <exception>
 
 //可以考虑让这个类的使用更优雅一点
 //不想折腾的删除这个定义
@@ -8,63 +9,191 @@
 template<class T>
 class LinkedList
 {
-	//...
+private:
+	struct Node
+	{
+		T data{};
+		Node* next{};
+		Node* prev{};
+	};
+	Node* m_front{};//始
+	Node* m_back{};//末
+	uint64_t m_count;//长度
+
+	//内部插入方法//向后插入
+	T* _InsertAfter(const T& data, Node* p_node)
+	{
+		Node* newnode = new Node;
+		newnode->data = data;
+
+		if (m_front)
+		{
+			//链表插入
+			newnode->next = p_node->next;
+			if (p_node->next)
+				p_node->next->prev = newnode;
+			else
+				m_back = newnode;
+			p_node->next = newnode;
+			newnode->prev = p_node;
+			m_count++;
+		}
+		else
+		{
+			//创建第一节点
+			newnode->next = nullptr;
+			newnode->prev = nullptr;
+			m_front = m_back = newnode;
+			m_count = 1;
+		}
+		return &newnode->data;
+	}
+	//内部插入方法//向前插入
+	T* _InsertBefore(const T& data, Node* p_node)
+	{
+		Node* newnode = new Node;
+		newnode->data = data;
+
+		if (m_front)
+		{
+			newnode->prev = p_node->prev;
+			if (p_node != m_front)
+				p_node->prev->next = newnode;
+			else
+				m_front = newnode;
+			p_node->prev = newnode;
+			newnode->next = p_node;
+			m_count++;
+		}
+		else
+		{
+			//创建第一节点
+			newnode->next = nullptr;
+			newnode->prev = nullptr;
+			m_front = m_back = newnode;
+			m_count = 1;
+		}
+		return &newnode->data;
+	}
 public:
 #ifdef LL_ITERATOR_FEATURE
+	//迭代器是对指针操作的抽象，外部使用时完全不需要关心指针
 	class iterator
 	{
 	private:
-		//...
+		Node* ptr{};
+		iterator(Node* ptr) :ptr(ptr) {}
+		friend class LinkedList;
 	public:
 		using iterator_concept = std::bidirectional_iterator_tag;
 		using difference_type = std::ptrdiff_t;
 		using value_type = T;
 		using pointer = T*;
 		using reference = T&;
-		reference operator*() const;
-		pointer operator->() const;
-		iterator& operator++();
-		iterator operator++(int);
-		iterator& operator--();
-		iterator operator--(int);
-		bool operator==(const iterator& other) const;
-		bool operator!=(const iterator& other) const;
-		reference operator[](difference_type  i) const;
+		reference operator*() const { return ptr->data; }
+		pointer operator->() const { return &ptr->data; }
+		iterator& operator++() { ptr = ptr->next; return *this; }
+		iterator operator++(int) { iterator tmp = *this; ptr = ptr->next; return tmp; }
+		iterator& operator--() { ptr = ptr->prev; return *this; }
+		iterator operator--(int) { iterator tmp = *this; ptr = ptr->prev; return tmp; }
+		iterator operator+(difference_type i) 
+		{ 
+			Node* ptr_ = ptr;
+			for (auto j = 0; j < i; j++)
+				ptr_ = ptr_->next;
+			return ptr_;
+		}
+		iterator operator-(difference_type i)
+		{
+			Node* ptr_ = ptr;
+			for (auto j = 0; j < i; j++)
+				ptr_ = ptr_->prev;
+			return ptr_;
+		}
+		bool operator==(const iterator& other) const { return ptr == other.ptr; }
+		bool operator!=(const iterator& other) const { return ptr != other.ptr; }
+		reference operator[](difference_type  i) const {}
 		auto operator<=>(const iterator&) const = default;
 	};
 
-	iterator begin();
-	iterator end();
-	iterator rbegin();
-	iterator rend();
+	iterator begin() { return { m_front }; }
+	iterator end() { return { nullptr }; }
+	iterator rbegin() { return { m_back }; }
+	iterator rend() { return { nullptr }; }
 #endif
 
 public:
-	LinkedList();
-	~LinkedList();
+	LinkedList() {}
+	LinkedList(std::initializer_list<T> dataArray)
+	{
+		for (auto& d : dataArray)
+		{
+			PushBack(d);
+		}
+	}
+	~LinkedList() { Clear(); }
 
-	T& operator[](int i);
+	T& operator[](uint64_t i) {
+		Node* p = m_front;
+		for (uint64_t j = 0; j < i; j++)
+		{
+			p = p->next;
+		}
+		return p->data;
+	}
 #ifdef LL_ITERATOR_FEATURE
 	//default insert after iter
-	void Insert(const T& val, const iterator& iter, bool insert_prev = false);
-	iterator Erase(const iterator& iter);
+	void Insert(const T& val, const iterator& iter, bool insert_prev = false) {
+		if (insert_prev) _InsertBefore(val, iter.ptr);
+		_InsertAfter(val, iter.ptr);
+	}
+	iterator Erase(const iterator& iter) {
+		Node* node = iter.ptr;
+		if (!node) return iter;//空操作
+		if (node == m_front)
+		{
+			m_front = node->next;
+		}
+		if (node == m_back)
+		{
+			m_back = node->prev;
+		}
+		if (node->prev)
+			node->prev->next = node->next;
+		if (node->next)
+			node->next->prev = node->prev;
+		m_count--;
+
+		if (!m_count) { m_front = m_back = nullptr; }
+		auto temp = node->next;
+		delete node;
+		return { temp };
+	}
 #else
 	//default insert after node
 	void Insert(const T& val, T* node, bool insert_prev = false);
 	T* Erase(T* node);
 #endif
 
-	T& Front();
-	T& Back();
-	void PushFront(const T& val);
-	void PushBack(const T& val);
-	void PopFront();
-	void PopBack();
+	T& Front() { return m_front->data; }
+	T& Back() { return m_back->data; }
+	void PushFront(const T& val) { _InsertBefore(val, m_front); }
+	void PushBack(const T& val) { _InsertAfter(val, m_back); }
+	void PopFront() { Erase( m_front ); }
+	void PopBack() { Erase(m_back); }
 
-	size_t Count();
-	bool Empty();
+	size_t Count() { return m_count; }
+	bool Empty() { return m_count == 0; }
 
-	void Clear();
+	void Clear(){
+		auto ptr = m_front;
+		while (ptr)
+		{
+			auto del = ptr;
+			ptr = ptr->next;
+			delete del;
+		}
+	}
 
 	//... other methods
 };
--- Week1/Week1.cpp
+++ Week1/Week1.cpp
@@ -1,8 +1,70 @@
 ﻿#include "LinkedList.h"
 #include "ForwardLinkedList.h"
+#include <iostream>
+#include <format>
 
-
+//可以在这里写上名字
 int main()
 {
+#ifdef FLL_ITERATOR_FEATURE
+
+	LinkedList<int> list{ 1,2,3,4,5,6,7,8,9,10 };
+
+	std::cout << "Elements in list\n";
+	for (auto& val : list)
+	{
+		std::cout << val << " ";
+		val += 1;
+	}
+	std::cout << std::endl;
+
+	//the same
+	std::cout << "Elements in list\n";
+	for (auto iter = list.begin(); iter != list.end(); iter++)
+	{
+		std::cout << *iter << " ";
+		*iter -= 1;
+	}
+	std::cout << std::endl;
+
+	std::cout << "Elements in list, inverse visit\n";
+	for (auto iter = list.rbegin(); iter != list.rend(); iter--)
+	{
+		std::cout << *iter << " ";
+		*iter += 1;
+	}
+	std::cout << std::endl;
+
+	struct MyStruct {
+		int x, y, z;
+		bool operator==(const MyStruct& other) { return x == other.x && y == other.y && z == other.z; }
+	};
+
+	LinkedList<MyStruct> list_struct{ {1,2,3},{4,5,6},{4,5,6},{7,8,9} };
+
+	std::cout << "Elements in list_struct\n";
+	for (auto& val : list_struct)
+		std::cout << std::format("x:{} y:{} z:{}\n", val.x, val.y, val.z);
+	std::cout << std::endl;
+
+	//same
+	std::cout << "Elements in list_struct\n";
+	for (auto iter = list_struct.begin(); iter != list_struct.end(); iter++)
+		std::cout << std::format("x:{} y:{} z:{}\n", iter->x, iter->y, iter->z);
+	std::cout << std::endl;
+
+	std::cout << "Erase nearby repeat element in list_struct\n";
+	for (auto iter = list_struct.begin(); iter != list_struct.end(); iter++)
+	{
+		if (iter + 1 != list_struct.end() &&*iter == *(iter + 1)) 
+			iter = list_struct.Erase(iter);
+
+		std::cout << std::format("x:{} y:{} z:{}\n", iter->x, iter->y, iter->z);
+	}
+	std::cout << std::endl;
+
+
+#endif
+
 }
 
